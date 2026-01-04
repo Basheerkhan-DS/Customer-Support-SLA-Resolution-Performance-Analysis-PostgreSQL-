ab4734da488e95bf8fa363239c8103b7a838fe5f
Basheerkhan-DS <pathabasheerkhan@gmail.com>
2026-01-04 10:54:07 +0530

    01_table_creation.sql

---
+++ sql/01_table_creation.sql
@@ -0,0 +1,21 @@
+--Creating a table to uplaod the data 
+
+CREATE TABLE customer_support(
+Ticket_ID INT,
+Customer_Name VARCHAR(50),
+Customer_Email VARCHAR(70),
+Age INT,
+Customer_Gender VARCHAR(10),
+Product_Purchased VARCHAR(200),
+Date_of_Purchase VARCHAR(50),
+Ticket_type VARCHAR(40),
+Ticket_Subject VARCHAR(60),
+Ticket_Description TEXT,
+Ticket_Status VARCHAR(30),
+Resolution VARCHAR(100),
+Ticket_Priority VARCHAR(20),
+Ticket_Channel VARCHAR(30),
+First_Response_Time VARCHAR(50),
+Time_to_Resolution VARCHAR(50),
+Customer_Satisfaction_Rating FLOAT
+);
